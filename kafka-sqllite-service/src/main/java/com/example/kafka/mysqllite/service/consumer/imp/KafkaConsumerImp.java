6b39ead22555efcd359327e65da180b720435f6b
saidaniamine12 <mohamedaminesaidani7@gmail.com>
2023-04-30 20:49:58 +0200

    first coppoommit

--- kafka-sqllite-service/src/main/java/com/example/kafka/mysqllite/service/consumer/imp/KafkaConsumerImp.java
+++ kafka-sqllite-service/src/main/java/com/example/kafka/mysqllite/service/consumer/imp/KafkaConsumerImp.java
@@ -2,8 +2,14 @@ package com.example.kafka.mysqllite.service.consumer.imp;
 
 import com.example.kafka.mysqllite.service.consumer.KafkaConsumer;
 import com.example.kafka.mysqllite.service.consumer.config.KafkaConfigData;
+
+import com.example.kafka.mysqllite.service.entities.Gps;
+import com.example.kafka.mysqllite.service.services.GpsService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -15,12 +21,14 @@ import java.util.List;
 
 @Service
 public class KafkaConsumerImp implements KafkaConsumer {
+    private final GpsService gpsService;
 
     private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumerImp.class);
 
     private final KafkaConfigData kafkaConfigData;
 
-    public KafkaConsumerImp(KafkaConfigData kafkaConfigData) {
+    public KafkaConsumerImp(GpsService gpsService, KafkaConfigData kafkaConfigData) {
+        this.gpsService = gpsService;
         this.kafkaConfigData = kafkaConfigData;
     }
     @Override
@@ -37,6 +45,17 @@ public class KafkaConsumerImp implements KafkaConsumer {
                 offsets.toString(),
                 Thread.currentThread().getId()
                 );
+
+        for (int i = 0; i < messages.size(); i++) {
+            messages.forEach(message -> LOG.info("saving :"+ message.toString()));
+            JsonObject jsonObject = new JsonParser().parse(messages.get(i)).getAsJsonObject();
+            Gps gps = new Gps(jsonObject.get("date_time").getAsString(),
+                    jsonObject.get("lat").getAsDouble(),
+                    jsonObject.get("log").getAsDouble());
+            gpsService.saveData(gps);
+        }
+
+
 
     }
 }
